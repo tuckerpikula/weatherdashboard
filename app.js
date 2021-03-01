3211b28bcd1b077232e82b58823518f57dcdfde6
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-02-28 20:32:53 -0800

    random push

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let cityAPI = JSON.parse(localStorage.getItem('saveCity')) || []
-cityAPI.className = 'grouplist'
+cityAPI.className = 'list-group-item'
 cityAPI.textContent = document.getElementById('search').value
 
 for (let i = 0; i < cityAPI.length; i++) {
   let cityAPI = document.createElement('li')
-  cityAPI.className = 'grouplist'
+  cityAPI.className = 'list-group-item'
   cityAPI.textContent = cityAPI[i]
   document.getElementById('cityList').append(cityAPI)
 }
@@ -21,17 +21,23 @@ document.getElementById('searchBtn').addEventListener('click', event => {
       let location = res.data
       console.log(location)
 
-      // let cityName = document.createElement('li')
-      // cityName.className = 'grouplist'
-      // cityName.textContent = document.getElementById('search').value
-      // document.getElementById('theCity').append(cityName)
+      let cityName = document.createElement('li')
+      cityName.className = 'list-group-item'
+      cityName.textContent = document.getElementById('search').value
+      document.getElementById('cityList').append(cityName)
 
-    //   document.getElementById('theCity').innerHTML = `
-    // <h2>${location.city.name}, ${currentTime.toDateString()}
-    // `
+      document.getElementById('theCity').innerHTML = `
+    <h1>${location.name}, ${time.toDateString()}</h1>
+    `
 
     })
     .catch(err => console.error(err))
 })
 
-// 6fefb9638e06563380186564cc975fce
+
+  // < h2 > ${ location.city.name }, ${ currentTime.toDateString() } <img src="http://openweathermap.org/img/wn/${location.list[0].weather[0].icon}@2x.png"></h2>
+  //           <p>Temperature: ${location.list[0].main.temp} °F</p>
+  //           <p>Humidity: ${location.list[0].main.humidity}%</P>
+  //           <p>Wind speed: ${location.list[0].wind.speed} MPH</P>
+  //           <p>UV index: </P>
+  //           <hr>
