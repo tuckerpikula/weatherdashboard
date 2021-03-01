78ef3a14e598b2cc1f87126d3cb50d069854ca0a
pikulatucker@gmail.com <pikulatucker@gmail.com>
2021-02-28 20:12:41 -0800

    working axios

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ let cityAPI = JSON.parse(localStorage.getItem('saveCity')) || []
 cityAPI.className = 'grouplist'
 cityAPI.textContent = document.getElementById('search').value
 
-for (let i = 0; i<cityAPI.length; i++) {
+for (let i = 0; i < cityAPI.length; i++) {
   let cityAPI = document.createElement('li')
   cityAPI.className = 'grouplist'
   cityAPI.textContent = cityAPI[i]
   document.getElementById('cityList').append(cityAPI)
 }
 
-document.getElementById('searchBTN').addEventListener('click', event => {
-  event.preventDefault()
+document.getElementById('searchBtn').addEventListener('click', event => {
+    event.preventDefault()
 
-  let name = document.getElementById('search').value
+  let cityName = document.getElementById('search').value
   let time = new Date()
 
 
-axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${name}&units=imperial&appid=a0facbfa14a51a09cfa5af1f8db212b0`)
-  .then(res => {
-    let location = res.data
-    console.log(location)
-  })
-  .catch(err => console.error(err))
+  axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=cde25f400fd71f13ed4e7af5635b553d`)
+    .then(res => {
+      let location = res.data
+      console.log(location)
+
+      // let cityName = document.createElement('li')
+      // cityName.className = 'grouplist'
+      // cityName.textContent = document.getElementById('search').value
+      // document.getElementById('theCity').append(cityName)
+
+    //   document.getElementById('theCity').innerHTML = `
+    // <h2>${location.city.name}, ${currentTime.toDateString()}
+    // `
+
+    })
+    .catch(err => console.error(err))
 })
 
+// 6fefb9638e06563380186564cc975fce
